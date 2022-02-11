087fb616f3aba1e4e56affe42292166510f99ba0
voeundavii <voeundavii@gmail.com>
2022-02-11 23:49:54 +0700

    Finish TD5

---
+++ java_script/Dinosaur_Game_script.js
@@ -0,0 +1,26 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+function jump() {
+    if (dino.classList != "jump") {
+        dino.classList.add("jump");
+
+        setTimeout(function() {
+            dino.classList.remove("jump");
+        }, 300);
+    }
+}
+
+let isAlive = setInterval(function() {
+    // get current dino Y position
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+
+    // get current cactus X position
+    let cactusLeft = parseInt(
+        window.getComputedStyle(cactus).getPropertyValue("left")
+    );
+}, 10);
+
+document.addEventListener("keydown", function(event) {
+    jump();
+});
